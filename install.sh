640d15cb2258b46ec629a7bc50a5f863a576dfe2
Augusto Mota Pinheiro <augustomp55@gmail.com>
2024-06-20 19:53:51 -0400

    feat: updated tmux config
    updated install script accordingly

--- install.sh
+++ install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 echo "Installing git, tar, wget & other essentials..."
-DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq -o=Dpkg::Use-Pty=0
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -qq -o=Dpkg::Use-Pty=0 tar unzip wget curl git build-essential
+DEBIAN_FRONTEND=noninteractive sudo apt-get update 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install tar unzip wget curl git build-essential
 
 echo "Installing nvim from GitHub at /opt/nvim..."
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz >/dev/null 2>&1
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz 
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz >/dev/null 2>&1
+sudo tar -C /opt -xzf nvim-linux64.tar.gz 
 rm nvim-linux64.tar.gz
 
 echo "Installing Miniconda at $HOME/miniconda3..." 
 mkdir -p $HOME/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh >/dev/null 2>&1
-bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3 >/dev/null 2>&1
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh 
+bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3 
 rm $HOME/miniconda3/miniconda.sh
 
 echo "Installing GitHub CLI..."
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 && sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null 2>&1\
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null 2>&1 \
-&& sudo apt-get update >/dev/null 2>&1\
-&& sudo apt-get install gh -y >/dev/null 2>&1
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list  \
+&& sudo apt-get update \
+&& sudo apt-get install gh -y 
 
 # Check if there's an authenticated user in GitHub CLI
-if gh auth status >/dev/null 2>&1; then
+if gh auth status ; then
     echo "Skipping GitHub CLI auth because it's already done!"
 else
     echo "Authenticating GitHub CLI..."
@@ -36,23 +36,26 @@ fi
 echo "Setting up git credentials from GitHub CLI..."
 gh auth setup-git
 
+echo "Installing xsel..."
+sudo apt-get install xsel
+
 echo "Installing tmux..."
-sudo apt-get install tmux >/dev/null 2>&1
+sudo apt-get install tmux 
 
 echo "Installing fzf from GitHub at $HOME/.fzf..."
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf >/dev/null 2>&1
-$HOME/.fzf/install --key-bindings --completion --no-update-rc >/dev/null 2>&1
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf 
+$HOME/.fzf/install --key-bindings --completion --no-update-rc 
 
 echo "Installing oh-my-posh from install script at $HOME/bin/oh-my-posh..."
 mkdir -p $HOME/bin
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/bin >/dev/null 2>&1
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/bin 
 
 echo "Downloading & applying personal dotfiles in $HOME..."
-git clone https://github.com/AugustDG/dotfiles.git $HOME/dotfiles --recursive >/dev/null 2>&1
+git clone https://github.com/AugustDG/dotfiles.git $HOME/dotfiles --recursive 
 sudo chown -R $USER $HOME/dotfiles
 sudo cp -r $HOME/dotfiles/. $HOME
 sudo rm -r $HOME/dotfiles
-sudo chown -R $USER .git .config/nvim/.git
+sudo chown -R $USER .git .config/nvim/.git .i3/.git
 
 echo "Autoremoving packages..."
-sudo apt-get autoremove -y >/dev/null 2>&1
+sudo apt-get autoremove -y 
